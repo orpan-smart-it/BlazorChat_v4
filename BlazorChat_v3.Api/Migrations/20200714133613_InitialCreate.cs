794a7f0a835882b537341fa44ccaa92a108f1cf6
orpan-smart-it <orpan@smart-it.com>
2020-07-14 17:51:27 +0300

    First upload
    first load. LocalDb was created in API project. Simple UI will be developed in Client part.

---
+++ BlazorChat_v3.Api/Migrations/20200714133613_InitialCreate.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorChat_v3.Api.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    ContactId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.ContactId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Contacts",
+                columns: new[] { "ContactId", "FirstName", "LastName" },
+                values: new object[] { 3, "Jack", "Rassel" });
+
+            migrationBuilder.InsertData(
+                table: "Contacts",
+                columns: new[] { "ContactId", "FirstName", "LastName" },
+                values: new object[] { 1, "Anatoliy", "Rassel" });
+
+            migrationBuilder.InsertData(
+                table: "Contacts",
+                columns: new[] { "ContactId", "FirstName", "LastName" },
+                values: new object[] { 2, "Lara", "Rassel" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
---
+++ BlazorChat_v3.Api/Models/ContactRepository.cs
@@ -0,0 +1,73 @@
+﻿using BlazorChat.Api.Models;
+using BlazorChat_v4.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorChat_v3.Api.Models
+{
+    public class ContactRepository : IContactRepository
+    {
+        private readonly AppDbContextClass appDbContext;
+        public ContactRepository(AppDbContextClass appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public async Task<Contact> AddContact(Contact contact)
+        {
+            //adding new contact
+            var result = await appDbContext.Contacts.AddAsync(contact);
+            //saving
+            await appDbContext.SaveChangesAsync();
+            //returning the result
+            return result.Entity;
+        }
+
+        public async void DeleteContact(int contactId)
+        {
+            var result = await appDbContext.Contacts
+                 .FirstOrDefaultAsync(e => e.ContactId == contactId);
+            if (result != null)
+            {
+                appDbContext.Contacts.Remove(result);
+                await appDbContext.SaveChangesAsync();
+            }
+        }
+
+        public async Task<Contact> GetContact(int contactId)
+        {
+            return await appDbContext.Contacts
+                .FirstOrDefaultAsync(e => e.ContactId == contactId);
+        
+        }
+
+        public async Task<IEnumerable<Contact>> GetContacts()
+        {
+            return await appDbContext.Contacts.ToListAsync();
+        }
+
+        public async Task<Contact> UpdateContact(Contact contact)
+        {
+            var result = await appDbContext.Contacts
+                .FirstOrDefaultAsync(e => e.ContactId == contact.ContactId);
+
+            if (result != null)
+            {
+                result.FirstName = contact.FirstName;
+                result.LastName = contact.LastName;
+
+                await appDbContext.SaveChangesAsync();
+                return result;
+
+
+            }
+            return null; 
+                
+
+        }
+    }
+}
